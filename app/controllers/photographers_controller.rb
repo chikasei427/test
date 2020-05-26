b7d95232417111bb84a8de6e98d54233e874af3b
EC2 Default User <ec2-user@ip-172-31-31-1.ec2.internal>
2020-05-26 03:23:31 +0000

    5/26 12:00

---
+++ app/controllers/photographers_controller.rb
@@ -0,0 +1,28 @@
+class PhotographersController < ApplicationController
+  def new
+    @photographer = Photographer.new
+  end
+  
+  def index
+  end
+  
+  def show
+  end
+  
+  def create
+    @photographer = Photographer.new(photographer_params)
+    if @photographer.save
+      # flash[:success] = 'ユーザを登録しました。'
+      redirect_to root_path
+    else
+      # flash.now[:danger] = 'ユーザの登録に失敗しました。'
+      render :new
+    end
+  end
+  
+ private
+  def photographer_params
+    params.require(:photographer).permit(:name, :email, :password, :password_confirmation)
+  end
+
+end
