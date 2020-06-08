8502b99204b1c57613208dd4e54d66e50861d0bd
EC2 Default User <ec2-user@ip-172-31-31-1.ec2.internal>
2020-06-08 07:37:58 +0000

    redirect_back

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -3,13 +3,13 @@ class FavoritesController < ApplicationController
         post = Post.find(params[:favorite_id])
         current_photographer.favorite(post)
         flash[:success] = '投稿をお気に入りに追加しました。'
-        redirect_to posts_path
+        redirect_back(fallback_location: root_path)
     end
-    
+
     def destroy
         post = Post.find(params[:favorite_id])
         current_photographer.unfavorite(post)
         flash[:success] = '投稿のお気に入りを解除しました。'
-        redirect_to posts_path
+        redirect_back(fallback_location: root_path)
     end
 end
